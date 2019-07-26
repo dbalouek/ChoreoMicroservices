8f7aaa2a7b25d586f6b779698864465fafa61650
Abhinav <abhinav.vedmala@gmail.com>
2019-07-26 13:58:58 -0400

    got service 1 and 2 working

--- functions/index.js
+++ functions/index.js
@@ -1,76 +1,40 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
+const request = require('request');
 
-app.post('/zips', async (req, res) => {
-    const zip = req.body.message;
-    try {
-        res.status(201).json({message: zip});
-    } catch(error) {
-        console.log('Error logging the zip code', error.message);
-        res.sendStatus(500);
-    }
-});
+admin.initializeApp(functions.config().firebase);
 
-exports.newZip = functions.firestore
-    .document('zips/{zipcode}')
-    .onCreate((snap, context) => {
-        const newValue = snap.data()
-        const zip = context.params.zipcode;
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?zip="+str(zip)+",us&units=imperial&appid=904eaa61902dba144dc85f29013ec210", true);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.onload = () => {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    let json = JSON.parse(xhr.responseText);
-                    const temp = json.main.temp;
-                    // upload temp to firestore
-                }
-            }
+let db = admin.firestore();
+
+exports.addZip = functions.https.onRequest(async (req, res) => {
+    const input = req.query.text;
+    let newZip = db.collection('zips').add({
+        zip_code: input
+    });
+    res.redirect(200);
+  });
+
+exports.addWeather = functions.firestore
+  .document('zips/{zipcode}')
+  .onCreate((snap, context) => {
+      const newValue = snap.data();
+      const apiKey = '904eaa61902dba144dc85f29013ec210';
+      const zipcode = newValue.zip_code;
+      const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&units=imperial&appid=${apiKey}`; 
+
+      return new Promise((resolve, reject) => {
+        request(url, (err, response, body) => {
+        if(err){
+          console.log('error:', error);
+        } else {
+          console.log('body:', body);
+          let weather = JSON.parse(body);
+          let newWeather = db.collection('weather').doc(zipcode).set({
+            city: weather.name,
+            temp: weather.main.temp
+          });
+          resolve();
         }
-        xhr.send(null);        
-    });
-
-exports.newWeather = functions.firestore
-    .document('weather/{zipcode}')
-    .onCreate((snap, context) => {
-        const newValue = snap.data()
-        const city = newValue.name;
-        const temp = newValue.temp;
-        const zip = context.params.zipcode;
-        // make tweet here  https://blog.rapidapi.com/how-to-use-the-twitter-api-with-javascript/
-    });
-
-app.get('/zips', async (req, res) => {
-    let query = admin.database().ref(`/zips`);
-
-    try {
-        const snapshot = await query.once('value');
-        let zips = [];
-        snapshot.forEach((childSnapshot) => {
-            zips.push({key: childSnapshot.key, message: childSnapshot.val().message});
         });
-
-        res.status(200).json(zips);
-    } catch(error) {
-        console.log('Error getting zip codes', error.message);
-        res.sendStatus(500);
-    }
-});
-
-// app.get('/zips/{zipcode}', async (req, res) => {
-//     const zipcode = req.params.zipcode;
-//     try {
-//         const snapshot = await admin.database().ref(`/zips/${zipcode}`).once('value');
-//         if (!snapshot.exists()) {
-//             return res.status(404).json({errorCode: 404, errorMessage: `message '${zipcode}' not found`});
-//         }
-//         return res.set('Cache-Control', 'private, max-age=300');
-//     } catch(error) {
-//         console.log('Error getting zipcode', zipcode, error.message);
-//         return res.sendStatus(500);
-//     }
-// });
-  
-exports.api = functions.https.onRequest(app);
+      });
+  });
