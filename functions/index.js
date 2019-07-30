e9b9306cc35691543841a7e6dd0430c3411d8f6e
Abhinav <abhinav.vedmala@gmail.com>
2019-07-30 09:47:34 -0400

    fixed the system to keep the functions warm

--- functions/index.js
+++ functions/index.js
@@ -7,11 +7,6 @@ admin.initializeApp(functions.config().firebase);
 
 let db = admin.firestore();
 
-exports.awake = functions.https.onRequest(async (req, res) => {
-  console.log('awake');
-  res.status(200).send('ok');
-});
-
 exports.addZip = functions.https.onRequest(async (req, res) => {
     const input = req.query.text;
     let FieldValue = require('firebase-admin').firestore.FieldValue;
@@ -20,7 +15,7 @@ exports.addZip = functions.https.onRequest(async (req, res) => {
         if(doc.exists) {
           const oldDate = doc.data().timestamp.toDate();
           var FIVE_MIN = 5 * 60 * 1000; /* ms */
-          if(((new Date) - oldDate) > FIVE_MIN) {
+          if(((new Date) - oldDate) > FIVE_MIN || input === 'awake') {
             let newZip = db.collection('zips').doc(input).set({
               zip_code: input,
               timestamp: FieldValue.serverTimestamp()
@@ -49,48 +44,58 @@ exports.addWeather = functions.firestore
       const apiKey = '904eaa61902dba144dc85f29013ec210';
       const zipcode = newValue.zip_code;
       const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&units=imperial&appid=${apiKey}`; 
-
-      return new Promise((resolve, reject) => {
-        request(url, (err, response, body) => {
-        if(err){
-          console.log('error:', err);
-          reject(Error('Could not get weather'));
-        } else {
-          let weather = JSON.parse(body);
-          let FieldValue = require('firebase-admin').firestore.FieldValue;
-          try {
-            let newWeather = db.collection('weather').doc(zipcode).set({
-              city: weather.name,
-              temp: weather.main.temp,
-              timestamp: FieldValue.serverTimestamp()
-            });
-            console.log('body:', body);
-          } catch(err){
-            console.log('error:', weather.message);
-          }
-          resolve();
-        }
+      let FieldValue = require('firebase-admin').firestore.FieldValue;
+      
+      if(zipcode === 'awake') {
+        let awake = db.collection('weather').doc('awake').set({
+          timestamp: FieldValue.serverTimestamp()
         });
-      });
+      } else {
+        return new Promise((resolve, reject) => {
+          request(url, (err, response, body) => {
+          if(err){
+            console.log('error:', err);
+            reject(Error('Could not get weather'));
+          } else {
+            let weather = JSON.parse(body);
+            try {
+              let newWeather = db.collection('weather').doc(zipcode).set({
+                city: weather.name,
+                temp: weather.main.temp,
+                timestamp: FieldValue.serverTimestamp()
+              });
+              console.log('body:', body);
+            } catch(err){
+              console.log('error:', weather.message);
+            }
+            resolve();
+          }
+          });
+        });
+      }
+      return 0;
   });
 
 exports.tweet = functions.firestore
   .document('weather/{weather}')
   .onWrite((change, context) => {
     const newValue = change.after.data();
-    const message = `It is ${newValue.temp} degrees in ${newValue.city}`;
-    var T = new Twit({
-      consumer_key:         'lZkuaQGAgdbLBJuMVOWsgNSkh',
-      consumer_secret:      'KGCkLI4tPTArxgDuVpgV25lJ34l5hY4uu9BoVzZiv8YPfJ5ino',
-      access_token:         '1108469359675084803-SVQapMQPKV2wYIS1iZlbedOCNxrx28',
-      access_token_secret:  'wEHj7K3KNHwE2oimNqGwwsJXtPrWHranUZShZ834X3nQY'
-    });
-    T.post('statuses/update', { status: message }, (err, data, response) => {
-      if(err){
-        console.log('error:', err)
-      } else {
-        console.log(data)
-      }
-    });
+
+    if(newValue.temp && newValue.city) {
+      const message = `It is ${newValue.temp} degrees in ${newValue.city}`;
+      var T = new Twit({
+        consumer_key:         'lZkuaQGAgdbLBJuMVOWsgNSkh',
+        consumer_secret:      'KGCkLI4tPTArxgDuVpgV25lJ34l5hY4uu9BoVzZiv8YPfJ5ino',
+        access_token:         '1108469359675084803-SVQapMQPKV2wYIS1iZlbedOCNxrx28',
+        access_token_secret:  'wEHj7K3KNHwE2oimNqGwwsJXtPrWHranUZShZ834X3nQY'
+      });
+      T.post('statuses/update', { status: message }, (err, data, response) => {
+        if(err){
+          console.log('error:', err)
+        } else {
+          console.log(data)
+        }
+      });
+    }
     return 0;
   });
